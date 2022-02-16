850d7a80d7b3f41218453a5c68418b7fabe3bb97
ilir pllana <ilirpllana95@gmail.com>
2022-02-15 20:52:50 -0500

    added product routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const userRoute = require('./routes/user')
+const productRoute = require('./routes/product')
 const authRoute = require('./routes/auth')
 
 const app = express()
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err) => console.log(err))
 
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
 app.use('/api/auth', authRoute);
 
 
--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,112 @@
-const router = require('express').Router()
+const Product = require('../models/Product');
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken');
+
+const router = require('express').Router();
 
 
+//CREATE PRODUCT
+router.post('/', verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body);
 
-module.exports = router
+    try {
+        const savedProduct = await newProduct.save()
+        res.status(200).json(savedProduct)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+//UPDATE PRODUCT
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true}
+        )
+        res.status(200).json(updatedProduct)
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+//DELETE PRODUCT
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id)
+
+        res.status(200).json("Product has been deleted")
+        
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+//GET PRODUCT
+
+router.get('/find/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        res.status(200).json(product)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+//GET ALL PRODUCTS
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+    const qNew = req.query.new; //checking if there is a query new=true
+    const qCategory = req.query.category; //checking if there is a query with category
+
+    try {
+        let products;
+
+        if(qNew) {
+            products = await Product.find().sort({ createdAt: -1 }).limit(5);
+        } else if(qCategory) {
+            products = await Product.find({
+                categories: {
+                    $in: [qCategory]
+                }
+            })
+        } else {
+            products = await Product.find();
+        } 
+        res.status(200).json(products)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+//GET USER STATS
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.getFullYear(date.getFullYear() - 1))
+
+    try {
+
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear}} },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                }
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: 1 }
+                }
+            }
+        ])
+        res.status(200).json(data)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router;
