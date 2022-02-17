65cec4f0d8c856f20590d84327b0d582a34cee0e
ilir pllana <ilirpllana95@gmail.com>
2022-02-17 10:54:34 -0500

    added all routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const userRoute = require('./routes/user')
 const productRoute = require('./routes/product')
 const authRoute = require('./routes/auth')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
 
 const app = express()
 
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO_URL)
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/orders', orderRoute);
 
 
 app.listen( process.env.PORT || 5000, () => console.log(`Server running successfully`))
